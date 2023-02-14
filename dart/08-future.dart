876a9b8257137e093f0b30bc87957e4e2f961b82
saurmo0108@gmail.com <saurmo0108@gmail.com>
2023-02-14 14:04:33 -0500

    Ejemplo http

---
+++ dart/08-future.dart
@@ -0,0 +1,17 @@
+import 'package:http/http.dart' as http;
+
+void main(List<String> args) {
+  String url = "api/v2/pokemon/ditto";
+  String host = "pokeapi.co";
+  httpGet(host, url).then((value) => null);
+}
+
+Future httpGet(String host, String path) async {
+  var url = Uri.http(host, path);
+
+  var response = await http.get(url);
+  print('Response status: ${response.statusCode}');
+  print('Response body: ${response.body}');
+
+  return response;
+}
