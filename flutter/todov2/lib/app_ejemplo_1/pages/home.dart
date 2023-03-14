92fa2f41d9c79741aa91587dfbbd051b5d4c045d
saurmo0108@gmail.com <saurmo0108@gmail.com>
2023-03-14 16:59:38 -0500

    Statefull

--- flutter/todov2/lib/app_ejemplo_1/pages/home.dart
+++ flutter/todov2/lib/app_ejemplo_1/pages/home.dart
@@ -1,25 +1,37 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _Home();
+  }
+}
 
-bool isActive=false;
-void onChanged1(bool? value){}
+class _Home extends State<Home> {
+  String clave = "";
+  bool? isActive = false;
+  void onChanged1(bool? value) {
+    setState(() {
+      isActive=value;
+      clave=value.toString();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(10),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-       Checkbox(value: false, onChanged: onChanged1),
-
-        ElevatedButton(
-          onPressed: () {},
-          child: Text("Click"),
-        ),
-      ]),
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Checkbox(value: isActive, onChanged: onChanged1),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Click"),
+            ),
+            Text(clave)
+          ]),
     );
   }
 }
