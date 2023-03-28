dd341381d9cd95987f0adb3a153691d06dc31977
saurmo0108@gmail.com <saurmo0108@gmail.com>
2023-03-28 17:04:05 -0500

    Call api with future builder

--- flutter/todov2/lib/app_ejemplo_1/controller/api_type.controller.dart
+++ flutter/todov2/lib/app_ejemplo_1/controller/api_type.controller.dart
@@ -1,37 +1,26 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:http/http.dart' as http;
 
 import 'package:todov2/app_ejemplo_1/controller/models/ApiType.dart';
 
 class ApiTypeController {
-  List<ApiType> apis = [
-    ApiType.fromJson({
-      "API": "AdoptAPet",
-      "Description": "Resource to help get pets adopted",
-      "Auth": "apiKey",
-      "HTTPS": true,
-      "Cors": "yes",
-      "Link": "https://www.adoptapet.com/public/apis/pet_list.html",
-      "Category": "Animals"
-    }),
-    ApiType.fromJson(
-      {
-        "API": "Axolotl",
-        "Description": "Collection of axolotl pictures and facts",
-        "Auth": "",
-        "HTTPS": true,
-        "Cors": "no",
-        "Link": "https://theaxolotlapi.netlify.app/",
-        "Category": "Animals"
-      },
-    )
-  ];
+  List<ApiType> apis = [];
 
-  List<ApiType> getApis()  {
+  showDetail(ApiType api) {
+    print(api.link);
+  }
+
+  Future<List<ApiType>> getApis() async {
     print("Call Api");
     var url = Uri.https('api.publicapis.org', 'entries');
-    // var response = await http.get(url);
-    print(apis);
+    var response = await http.get(url);
+    String data = response.body;
+    Map<String, dynamic> jsonApis = jsonDecode(data);
+    List entries = jsonApis["entries"];
+    entries.forEach((element) {
+      apis.add(ApiType.fromJson(element));
+    });
     return apis;
   }
 }
--- flutter/todov2/lib/app_ejemplo_1/pages/apis_free.dart
+++ flutter/todov2/lib/app_ejemplo_1/pages/apis_free.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:todov2/app_ejemplo_1/controller/api_type.controller.dart';
+import 'package:todov2/app_ejemplo_1/controller/models/ApiType.dart';
 
 class ApisFree extends StatefulWidget {
   @override
@@ -13,19 +15,68 @@ class _ApisFree extends State {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      itemBuilder: fillListView,
-      separatorBuilder: (context, index) => Divider(),
-      itemCount: apiTypeController.apis.length,
+    return Center(
+      child: FutureBuilder(
+        future: apiTypeController.getApis(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return const CircularProgressIndicator();
+            case ConnectionState.done:
+              if (snapshot.hasData) {
+                List<ApiType>? data = snapshot.data;
+                if (data != null) {
+                  return createListView(data);
+                } else {
+                  return Text("Error cargando la lista.");
+                }
+              } else {
+                return Text("No hay datos para mostrar");
+              }
+
+            default:
+              return Text("Ha ocurrido un error inesperado.");
+          }
+        },
+      ),
     );
   }
 
-  Widget fillListView(context, index) {
-     var apis = apiTypeController.getApis();
- 
-    return ListTile(
-        title: Text(apis[index].description),
-        subtitle: Text("${apis[index].api} - ${apis[index].link}"),
-      );
+  Widget createListView(List<ApiType> apis) {
+    return ListView.separated(
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(apis[index].description),
+          subtitle: Text("${apis[index].api} - ${apis[index].link}"),
+          onTap: () {
+            apiTypeController.showDetail(apis[index]);
+          },
+          onLongPress: () {
+            showDialog(
+              context: context,
+              barrierDismissible: false,
+              builder: (context) {
+                return AlertDialog(
+                  title: Text("Desea eliminar el elemento?"),
+                  content: Text("El elemento no se puede recuperar"),
+                  actions: [
+                    TextButton(
+                      onPressed: () => Navigator.pop(context),
+                      child: Text("No"),
+                    ),
+                    TextButton(
+                      onPressed: () => Navigator.pop(context),
+                      child: Text("Si"),
+                    )
+                  ],
+                );
+              },
+            );
+          },
+        );
+      },
+      separatorBuilder: (context, index) => Divider(),
+      itemCount: apis.length,
+    );
   }
 }
