8ee7f4eeab5816f9ea574a46b0f8ecbcb0159020
saurmo0108@gmail.com <saurmo0108@gmail.com>
2023-05-09 16:05:49 -0500

    - feat: creacion de proyecto auth

---
+++ flutter/auth-app/authapp/lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:authapp/services/firebase_service.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: FutureBuilder(
+          future: FirebaseService.firebaseInit(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () async {
+                      await FirebaseService.signInGoogle();
+                    },
+                    child: Text("Iniciar con google"),
+                  ),
+                ],
+              );
+            } else {
+              return const CircularProgressIndicator();
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ flutter/auth-app/authapp/lib/services/firebase_service.dart
@@ -0,0 +1,43 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+
+class FirebaseService {
+  static Future<FirebaseApp?> firebaseInit() async {
+    try {
+      FirebaseApp app = await Firebase.initializeApp();
+      return app;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  static Future<User?> signInGoogle() async {
+    try {
+      // Trigger the authentication flow
+
+GoogleSignIn signIn = GoogleSignIn(scopes: ["email"]);
+
+      final GoogleSignInAccount? googleUser = await signIn.signIn();
+
+      // Obtain the auth details from the request
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+
+      // Create a new credential
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      // Once signed in, return the UserCredential
+      UserCredential userCredential = await FirebaseAuth.instance.signInWithCredential(credential);
+
+      return userCredential.user;
+    
+    } catch (e) {
+      return null;
+    }
+  }
+}
