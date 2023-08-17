bc992cdafc3ab4581c7904977e5a22f2d70595e4
saurmo <saurmo0108@gmail.com>
2023-08-17 17:40:30 -0500

    feat: bases flutter

--- apptareas20232/README.md
+++ apptareas20232/README.md
@@ -1,16 +1,22 @@
 # apptareas20232
 
-A new Flutter project.
 
-## Getting Started
+* MaterialApp
+    * Scaffold
+        body:
+        appBar:
 
-This project is a starting point for a Flutter application.
+# Widgets
+* Text
+* Container
+* Column
+* Row
 
-A few resources to get you started if this is your first Flutter project:
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+****
+* Images
+* ListView
+* Buttons
+* Forms
+
--- apptareas20232/lib/main.dart
+++ apptareas20232/lib/main.dart
@@ -1,29 +1,84 @@
-import 'package:apptareas20232/Home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  const myApp = MyApp();
-  runApp(myApp);
+  runApp(AppBases());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class AppBases extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    String title = 'Mi Aplicación';
-    Color colorApp = const Color.fromRGBO(150, 50, 30, 0.5);
-    ThemeData theme = ThemeData(
-      colorScheme: ColorScheme.fromSeed(seedColor: colorApp),
-      useMaterial3: true,
-    );
-    print("Inicio de app");
-    MyHomePage home =  MyHomePage();
-
-    return MaterialApp(
-      title: title,
-      theme: theme,
-      home: home,
-    );
+    return MaterialApp(home: createScaffold());
   }
+}
+
+createScaffold() {
+  return Scaffold(
+    appBar: AppBar(
+      title: const Text("Aplicación 2023 2"),
+    ),
+    body: Container(
+      margin: EdgeInsets.all(50),
+      decoration: BoxDecoration(
+        color: Colors.amber,
+        border: Border.all(
+          color: Colors.black,
+        ),
+      ),
+      child: createColum(),
+    ),
+  );
+}
+
+Column createColum() {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      createRow(),
+      createRow(),
+      Text("...."),
+      Image.network(
+          "https://cdn.pixabay.com/photo/2023/08/08/17/20/yosemite-8177850_1280.jpg"),
+    ],
+  );
+}
+
+createRow() {
+  return Container(
+    decoration: BoxDecoration(
+      color: Colors.blueGrey,
+      border: Border.all(width: 1),
+    ),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        createContainer(text: "1"),
+        createContainer(text: "2", color: Colors.green),
+        createContainerWithDecoration(text: "3")
+      ],
+    ),
+  );
+}
+
+Container createContainerWithDecoration({required text, Color? color}) {
+  return Container(
+    decoration: BoxDecoration(
+      color: Colors.black45,
+      border: Border.all(
+        width: 1,
+        color: Colors.red,
+      ),
+    ),
+    width: 50,
+    height: 50,
+    child: Text(text),
+  );
+}
+
+createContainer({required text, Color? color}) {
+  return Container(
+    color: color ?? Colors.blue,
+    width: 50,
+    height: 50,
+    child: Text(text),
+  );
 }
