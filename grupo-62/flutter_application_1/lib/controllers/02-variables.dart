81c055ef2bd55a06d81b304c942ec519d8b286a5
saurmo0108@gmail.com <saurmo0108@gmail.com>
2024-02-12 06:32:14 -0500

    feat: grupo 62

---
+++ grupo-62/flutter_application_1/lib/controllers/02-variables.dart
@@ -0,0 +1,58 @@
+// ignore_for_file: unused_local_variable
+
+void main() {
+  // VARIABLES EXPLICITAS
+  // Básicos
+  String nombre = 'Carlos';
+  int anio = 2023;
+  double diametro = 1.70;
+  bool esPositivo = true;
+
+// Colecciones
+  List<String> paises = ["Colombia"];
+  Set<String> names2 = {};
+  Map<int, String> nobleGases2 = {
+    2: 'helium',
+    10: 'neon',
+    18: 'argon',
+  };
+  Map<String, dynamic> images = {
+    'tags': ['saturn'],
+    'url': '//path/to/saturn.jpg',
+    'tag1': 12,
+    "tag2": { "name": "test" }
+  };
+
+  // VARIABLE IMPLICITAS
+  var name = 'Santiago';
+   // name = 12; // No se puede cambiar el tipo de dato
+  const year = 1977;
+  // year = 200; // No se puede cambiar el valor de la variable
+
+  dynamic diameter = 3.7;
+  diameter = "TEST"; // Si se puede cambiar el valor y el ripo
+
+  var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  var names = <String>{};
+  var isReal = false;
+  var image = {
+    'tags': ['saturn'],
+    'url': '//path/to/saturn.jpg'
+  };
+  var nobleGases = {
+    2: 'helium',
+    10: 'neon',
+    18: 'argon',
+  };
+
+// Valores por defecto (null)
+// Null safety
+  String? testerName;
+  print(testerName);
+
+  int valorSemestre = 50000000;
+  print("UdeM $valorSemestre");
+  print("UdeM \$$valorSemestre");
+
+
+}
---
+++ grupo-62/flutter_application_1/lib/controllers/Untitled-1.md
@@ -0,0 +1,9 @@
+
+Requisitos:
+- Android studio actualizado.
+    - Revisar que quede con el sdk instalado.
+- Visual studio code actualizado.
+
+Instrucciones de instalación flutter.
+
+1. 
